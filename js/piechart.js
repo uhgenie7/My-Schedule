d27cd195c8612a62fb008c33e51b400fa2233e7f
hyejin <dzzd777@naver.com>
2021-02-10 12:50:11 +0900

    2/10 파이차트 json 파싱 후 갱신해주는 메서드 추가. ajaxComplete.

--- js/piechart.js
+++ js/piechart.js
@@ -1,43 +1,44 @@
 $(function () {
-  let pieSize = 250;
-  let liWidth = $(window).width();
-  let clear;
-
-  if (liWidth > 401) {
-    pieSize = 250;
-  } else {
-    pieSize = 180;
-  }
-
-  $('.circle-graph').easyPieChart({
-    scaleColor: false,
-    lineWidth: 40,
-    lineCap: 'butt',
-    barColor: '#003A79',
-    trackColor: '#3b5999' ,
-    size: pieSize,
-    animate: 800
-  });
-
-  $(window).resize(function () {
+  $(window).ajaxComplete(function () {
+    let pieSize = 250;
     let liWidth = $(window).width();
-    if (liWidth > 400) {
+    let clear;
+
+    if (liWidth > 401) {
       pieSize = 250;
     } else {
       pieSize = 180;
     }
-    clear = setTimeout(function () {
-        $('.circle-graph').removeData('easyPieChart').find('canvas').remove();
-        $('.circle-graph').easyPieChart({
-        scaleColor: false,
-        lineWidth: 40,
-        lineCap: 'butt',
-        barColor: '#003A79',
-        trackColor: '#3b5999' ,
-        size: pieSize,
-        animate: 1
-        });
-      console.log(1)
-     }, 150);
+
+    $('.circle-graph').easyPieChart({
+      scaleColor: false,
+      lineWidth: 40,
+      lineCap: 'butt',
+      barColor: '#003A79',
+      trackColor: '#3b5999' ,
+      size: pieSize,
+      animate: 800
+    });
+
+    $(window).resize(function () {
+      let liWidth = $(window).width();
+      if (liWidth > 400) {
+        pieSize = 250;
+      } else {
+        pieSize = 180;
+      }
+      clear = setTimeout(function () {
+          $('.circle-graph').removeData('easyPieChart').find('canvas').remove();
+          $('.circle-graph').easyPieChart({
+          scaleColor: false,
+          lineWidth: 40,
+          lineCap: 'butt',
+          barColor: '#003A79',
+          trackColor: '#3b5999' ,
+          size: pieSize,
+          animate: 1
+          });
+      }, 150);
+    });
   });
 });
