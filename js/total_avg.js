4aa39c544c6264c308b45daf60c4e4cd567c93a5
hyejin <dzzd777@naver.com>
2021-02-10 12:50:55 +0900

    2/10 파이차트 토탈 갱신 json 작업 및 index.php 파일에 append 작업.

---
+++ js/total_avg.js
@@ -0,0 +1,19 @@
+$(function () {
+  $.ajax({
+    url: '/myschedule/data/sch_rate.json',
+    success: function (result) {
+      let apiRate = Number(result[0].api_rate);
+      let dbRate = Number(result[0].db_rate);
+      let plaRate = Number(result[0].pla_rate);
+      let renRate = Number(result[0].ren_rate);
+
+      let schAvg = (apiRate * 0.2) + (dbRate * 0.4) + (plaRate * 0.3) + (renRate * 0.1)
+      console.log(schAvg);
+
+      $(".circle-graph-container").append(`<div class="circle-graph easyPieChart" data-percent="${schAvg}">
+        <p>${schAvg}%</p>
+      </div>`)
+    }
+  });
+});
+
