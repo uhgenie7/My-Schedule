f0d3734611b8ec2f62b67ac97aeb05a5ea456d95
hyejin <dzzd777@naver.com>
2021-02-16 12:40:52 +0900

    2/16 추가: 타이틀 제목에 a 태그 추가 타이틀 제목을 눌렀을 때 메인 페이지로 돌아갈 수 있도록 a 태그를 추가하였다

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <header>
   <div class="center headerWrap">
     <a href="/myschedule/index.php"><i class="fa fa-home"></i></a>
-    <h1 id="title">Schedule Dashboard</h1>
+    <h1><a href="/myschedule/index.php" id="title">Schedule Dashboard</a></h1>
     <div class="mIcon">
       <span></span>
       <span></span>
