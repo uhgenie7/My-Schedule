2c681225e6b3c2fdc586f9b719065740ad336869
hyejin <dzzd777@naver.com>
2021-02-16 12:44:22 +0900

    2/16 추가: 카테고리 버튼 클릭 시 해당되는 php로 이동되는 페이지 각각 추가 해당되는 탭에는 active를 넣어주어 탭 버튼 색을 바꾸어 줌 데이터베이스에서 해당되는 테이블을 찾아 그 부분만 출력되도록 하는 기능

---
+++ include/view_api.php
@@ -0,0 +1,39 @@
+  <div class="categoryTabs">
+    <a href="/myschedule/pages/sch_view.php?key=view_all">All</a>
+    <a href="/myschedule/pages/sch_view.php?key=view_db">Database</a>
+    <a href="/myschedule/pages/sch_view.php?key=view_api" class="active">API</a>
+    <a href="/myschedule/pages/sch_view.php?key=view_renewal">Renewal</a>
+    <a href="/myschedule/pages/sch_view.php?key=view_web">Web Planning</a>
+  </div>
+  <ul class="boardTable">
+    <li class="boardTitle">
+      <span class="boNum">번호</span>
+      <span class="boCate">종류</span>
+      <span class="boTit">제목</span>
+      <span class="boReg">작성일</span>
+    </li>
+
+  <?php
+    include $_SERVER["DOCUMENT_ROOT"]."/myschedule/include/db_conn.php";
+    $sql = "select * from sch_txt where sch_txt_cate='ApiProject' order by sch_txt_num desc";
+    $board_result=mysqli_query($dbConn, $sql);
+    while($board_row=mysqli_fetch_array($board_result)){
+      $bo_num=$board_row['sch_txt_num'];
+      $bo_cate=$board_row['sch_txt_cate'];
+      $bo_tit=$board_row['sch_txt_tit'];
+      $bo_reg=$board_row['sch_txt_reg'];
+  ?>
+
+    <li class="boardList">
+      <span class="boNum"><?=$bo_num?></span>
+      <span class="boCate"><?=$bo_cate?></span>
+      <span class="boTit"><a href="#"><?=$bo_tit?></a></span>
+      <span class="boReg"><?=$bo_reg?></span>
+    </li>
+  <?php
+    }
+  ?>
+  </ul>
+  <div class="loadMore">
+    <button type="button">더보기</button>
+  </div>
---
+++ pages/sch_view.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Input Board</title>
+    <!-- awesome font -->
+    <!-- <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    /> -->
+    <script src="https://kit.fontawesome.com/4d645dbcdc.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="/myschedule/css/reset.css" />
+    <link rel="stylesheet" href="/myschedule/css/style.css" />
+    <link rel="stylesheet" href="/myschedule/css/media.css" />
+  </head>
+  <body>
+    <!-- all contents are wrapped inside wrap -->
+    <div class="wrap">
+      <?php
+        include $_SERVER["DOCUMENT_ROOT"]."/myschedule/include/header.php"
+      ?>
+      <!-- end of header -->
+      <!-- php line start-->
+      <?php
+        include $_SERVER["DOCUMENT_ROOT"]."/myschedule/include/db_conn.php";
+        $sql = "select * from schedule_progress";
+
+        $sch_result=mysqli_query($dbConn, $sql);
+        $sch_row=mysqli_fetch_array($sch_result);
+
+        $sch_db=$sch_row['sch_db'];
+        $sch_api=$sch_row['sch_api'];
+        $sch_ren=$sch_row['sch_ren'];
+        $sch_pla=$sch_row['sch_pla'];
+      ?>
+      <!-- php line end-->
+      <div class="center gridWrap">
+          <div class="inputContainer">
+              <?php
+                include $_SERVER["DOCUMENT_ROOT"]."/myschedule/include/latest_date.php";
+                include $_SERVER["DOCUMENT_ROOT"]."/myschedule/include/grid_up.php";
+              ?>
+              <div class="item boardBox">
+              <?php
+                $include_path=$_GET['key'];
+                include $_SERVER["DOCUMENT_ROOT"]."/myschedule/include/$include_path.php";
+              ?>
+              </div>
+              <!-- end of grid_up -->
+              <div class="item btns">
+                <button type="button" class="submitBtn" onclick="javascript:location.href='/myschedule/pages/input_form.php'">진행 상황 작성</button>
+              </div>
+              <!-- end of button -->
+          </div>
+          <!-- end of contatiner -->
+      </div>
+      <!-- end of center -->
+      <?php
+        include $_SERVER["DOCUMENT_ROOT"]."/myschedule/include/footer.php"
+      ?>
+      <!-- end of footer -->
+    </div>
+    <!-- end of wrap -->
+
+    <!-- script files load -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.6/jquery.easypiechart.min.js"></script>
+    <script src="/myschedule/js/piechart.js"></script>
+    <script src="/myschedule/js/custom.js"></script>
+    <script src="/myschedule/js/total_avg.js"></script>
+    <script>
+      $(".boardList").hide();
+      $(".boardList").slice(0, 5).show();
+      $(".loadMore button").click(function(){
+        $(".boardList:hidden").slice(0, 5).show();
+      });
+    </script>
+  </body>
+</html>
