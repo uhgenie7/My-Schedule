3fc3da6fe7546253ca3ed21ffa05dd8a9fe9b700
hyejin <dzzd777@naver.com>
2021-02-16 16:21:47 +0900

    2/16 생성: 게시물 삭제 페이지 게시물을 삭제했을 때 프로세스 파일

---
+++ php/detail_delete.php
@@ -0,0 +1,13 @@
+<?php
+  $del_num=$_GET['num'];
+  include $_SERVER["DOCUMENT_ROOT"]."/myschedule/include/db_conn.php";
+  $sql="delete from sch_txt where sch_txt_num=$del_num";
+  mysqli_query($dbConn, $sql);
+
+  echo "
+  <script>
+    alert('삭제가 완료되었습니다');
+    location.href='/myschedule/pages/sch_view.php?key=view_all';
+  </script>
+  ";
+?>
