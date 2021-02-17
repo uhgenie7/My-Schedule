609deb1cb67dbb73ea7e5b935f3e0941d1a5baa9
hyejin <dzzd777@naver.com>
2021-02-17 10:52:10 +0900

    2/17 추가: 내비게이션 추가, 스타일 작업 중 내비게이션 추가.

--- include/header.php
+++ include/header.php
@@ -2,10 +2,26 @@
   <div class="center headerWrap">
     <a href="/myschedule/index.php"><i class="fa fa-home"></i></a>
     <h1><a href="/myschedule/index.php" id="title">Schedule Dashboard</a></h1>
-    <div class="mIcon">
+    <div class="mIcon on">
       <span></span>
       <span></span>
       <span></span>
+    </div>
+    <div class="hiddenHeader">
+      <ul class="depth-1">
+        <li><a href="#"><i class="fa fa-home"></i></a></li>
+        <li><a href="#"><i class="fa fa-pencil"></i></a></li>
+        <li>
+          <a href="#"><i class="fa fa-search"></i></a>
+          <ul class="depth-2">
+            <li><a href="#">ALL</a></li>
+            <li><a href="#">Database</a></li>
+            <li><a href="#">API</a></li>
+            <li><a href="#">Renewal</a></li>
+            <li><a href="#">WEB Planning</a></li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </div>
 </header>
