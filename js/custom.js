b1ede572ae03fe6414d41e60ecc5bfe3a1728ee0
hyejin <dzzd777@naver.com>
2021-02-17 18:16:42 +0900

    2/17 추가: 내비게이션 토글 기능, 수정 버튼 클릭 시 버튼 show, hide  기능 추가 내비게이션 토글 기능: 바닐라 자바스크립트로 구현하였다. mIcon class에 on이 있으면 제거, 없으면 추가해주며 조건에 따라 depth가 열리도록 하였다. 내비게이션 토글이 열렸을 때 배경을 클릭하면 닫힐 수 있는 이벤트 기능도 같이 추가하였다.
    버튼 on/off 기능 추가: 조건에 따라 해당되는 스타일이 보이거나 보이지 않는 기능이 추가되었다. DOM을 여러번 써주어야 해서 제이쿼리로 구현하였다.

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,60 @@
 $(function () {
   // sub projects bar animation
-  for (let i = 0; i < $(".subPfBar").length; i++){
+  for (let i = 0; i < $(".subPfBar").length; i++) {
     let rateNum = $(".subPfBar").eq(i).find(".rateNum").val();
     $(".subPfBar").eq(i).find(".pfBar").animate({ width: rateNum + "%" }, 1200);
   }
+
+ //header icon control
+  // jquery 방식
+
+  // $(".mIcon").click(function (e) {
+  //   e.stopPropagation();
+  //   $(this).toggleClass("on");
+
+  //   if($(this).hasClass("on")){
+  //     $(".depth-1").slideDown('fast');
+  //   } else {
+  //     $(".depth-1").slideUp('fast');
+  //   }
+  // });
+
+  // $(document).click(function () {
+  //   $(".mIcon").toggleClass("on");
+  //   $(".depth-1").hide();
+  // })
+
+  // hidden input show and hide
+  $(".updateConBtn").click(function () {
+    $(this).toggleClass("on");
+    if ($(this).hasClass("on")) {
+      $(this).text("수정 취소");
+      $(".hiddenTit, .hiddenCon, .subBtn").show();
+      $(".boTit em, .boCon em").hide();
+      } else {
+        $(this).text("수정");
+      $(".hiddenTit, .hiddenCon, .subBtn").hide();
+      $(".boTit em, .boCon em").show();
+    }
+  })
+});
+
+let mIcon = document.querySelector(".mIcon");
+let depth_1 = document.querySelector(".depth-1")
+
+function navi(e) {
+  e.stopPropagation();
+  let hasClass = mIcon.classList.contains("on");
+  mIcon.classList.toggle("on");
+  if (!hasClass) {
+    depth_1.style.display = "block";
+  } else {
+    depth_1.style.display = "none";
+  }
+}
+
+mIcon.addEventListener("click", navi);
+document.addEventListener("click", function () {
+  mIcon.classList.remove("on");
+  depth_1.style.display = "none";
 });
