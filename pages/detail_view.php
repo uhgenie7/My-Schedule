f9ca65307e1900b13783535e7393c4f249cf6dc5
hyejin <dzzd777@naver.com>
2021-02-17 18:17:53 +0900

    2/17 추가: 수정 기능 추가 수정 버튼을 누르면 제목과 내용이 수정할 수 있도록 칸이 생긴다.

--- pages/detail_view.php
+++ pages/detail_view.php
@@ -5,10 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Input Board</title>
     <!-- awesome font -->
-    <!-- <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-    /> -->
     <script src="https://kit.fontawesome.com/4d645dbcdc.js" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="/myschedule/css/reset.css" />
@@ -48,10 +44,6 @@
                       include $_SERVER["DOCUMENT_ROOT"]."/myschedule/include/db_conn.php";
                       $sql = "select * from sch_txt where sch_txt_num= $detail_num";
                       $board_result=mysqli_query($dbConn, $sql);
-
-                      // $num = mysqli_num_rows($board_result);
-                      // db게시판에 존재하는 글 갯수
-                      // $표시번호 = $총게시물수 - (($표시되는 페이지수 - 1) * $표시되는 게시물수)
                       while($board_row=mysqli_fetch_array($board_result)){
                         $bo_num=$board_row['sch_txt_num'];
                         $bo_cate=$board_row['sch_txt_cate'];
@@ -60,35 +52,48 @@
                         $bo_con=$board_row['sch_txt_con'];
                   ?>
                 <div class="detailTit">
-                  <h2><?=$bo_cate?></h2>
+                  <form action="/myschedule/php/detail_update.php?num=<?=$bo_num?>" method="post">
+                  <!-- get방식으로 보내면 textarea가 길어지면 주소창이 더러워짐 -->
+                    <div class="detailBox">
+                      <h2><?=$bo_cate?></h2>
+                    </div>
+                      <ul class="viewTable">
+                        <li class="viewTitle">
+                          <span class="boNum">번호</span>
+                          <span class="boTit">제목</span>
+                          <span class="boCon">내용</span>
+                          <span class="boReg">작성일</span>
+                        </li>
+                        <li class="viewList">
+                          <span class="boNum"><?=$bo_num?></span>
+                          <span class="boTit">
+                            <input type="text" value="<?=$bo_cate?>" class="hiddenTit" name="updateTit">
+                            <em><?=$bo_tit?></em>
+                          </span>
+                          <span class="boCon">
+                            <em><?=$bo_con?></em>
+                            <textarea class="hiddenCon" name="updateCon"><?=$bo_con?></textarea>
+                          </span>
+                          <span class="boReg"><?=$bo_reg?></span>
+                        </li>
+                        <?php
+                          }
+                        ?>
+                      </ul>
+                      <div class="subBtn">
+                        <button type="submit">수정 사항 입력</button>
+                      </div>
+                  </form>
+                    </div>
+                  <!-- end of grid_up -->
+                  <div class="item btns">
+                    <button class="updateConBtn">수정</button>
+                    <button onclick="confirmDel()">삭제</button>
+                    <button type="button" class="submitBtn" onclick="javascript:location.href='/myschedule/pages/sch_view.php?key=view_all'">진행 상황 확인</button>
+                  </div>
+                  <!-- end of button -->  
                 </div>
-                  <ul class="viewTable">
-                    <li class="viewTitle">
-                      <span class="boNum">번호</span>
-                      <span class="boTit">제목</span>
-                      <span class="boCon">내용</span>
-                      <span class="boReg">작성일</span>
-                    </li>
-
-                   
-                    <li class="viewList">
-                      <span class="boNum"><?=$bo_num?></span>
-                      <span class="boTit"><?=$bo_tit?></span>
-                      <span class="boCon"><em><?=$bo_con?></em></span>
-                      <span class="boReg"><?=$bo_reg?></span>
-                    </li>
-                    <?php
-                      }
-                    ?>
-                  </ul>
-              </div>
-              <!-- end of grid_up -->
-              <div class="item btns">
-                <button>수정</button>
-                <button onclick="confirmDel()">삭제</button>
-                <button type="button" class="submitBtn" onclick="javascript:location.href='/myschedule/pages/sch_view.php?key=view_all'">진행 상황 확인</button>
-              </div>
-              <!-- end of button -->
+                <!-- end of detailTit -->
           </div>
           <!-- end of contatiner -->
       </div>
