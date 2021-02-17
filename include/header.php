5ff6aca208dad7790ff69614e06255536b935bec
hyejin <dzzd777@naver.com>
2021-02-17 18:14:07 +0900

    2/17 수정: 헤더의 아이콘 및 기능 변경 헤더의 홈 이미지를 로그아웃 이미지로 변경하여 로그아웃 기능을 구현하였다.

--- include/header.php
+++ include/header.php
@@ -1,18 +1,32 @@
+<?php
+  session_start();
+  if(isset($_SESSION['usercode'])){
+    $usercode=$_SESSION['usercode'];
+    // setcookie('PHPSESSID', '4u1op30l7cjeeimbq84lgf2qff', time() + 600, '/');
+  } else {
+    echo "
+    <script>
+      location.href='/myschedule/pages/auth_form.php';
+    </script>
+    ";
+  }
+?>
+
 <header>
   <div class="center headerWrap">
-    <a href="/myschedule/index.php"><i class="fa fa-home"></i></a>
+    <a href="/myschedule/php/sign_out.php"><i class="fa fa-sign-out"></i></a>
     <h1><a href="/myschedule/index.php" id="title">Schedule Dashboard</a></h1>
-    <div class="mIcon on">
+    <div class="mIcon">
       <span></span>
       <span></span>
       <span></span>
     </div>
     <div class="hiddenHeader">
       <ul class="depth-1">
-        <li><a href="#"><i class="fa fa-home"></i></a></li>
-        <li><a href="#"><i class="fa fa-pencil"></i></a></li>
+        <li><a href="/myschedule/index.php"><i class="fa fa-home"></i></a></li>
+        <li><a href="/myschedule/pages/input_form.php"><i class="fa fa-pencil"></i></a></li>
         <li>
-          <a href="#"><i class="fa fa-search"></i></a>
+          <a href="/myschedule/pages/sch_view.php?key=view_all"><i class="fa fa-search"></i></a>
           <ul class="depth-2">
             <li><a href="#">ALL</a></li>
             <li><a href="#">Database</a></li>
