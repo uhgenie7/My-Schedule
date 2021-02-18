79aa2fe417d25933ee54fd3d405746d722585221
hyejin <dzzd777@naver.com>
2021-02-18 10:31:46 +0900

    2/18 추가: 캐시 메타 추가 단점: 로딩 속도가 느려질 수 있는 문제

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-realidate">
+    <!-- 로딩속도가 느려질 수 있음... -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="apple-touch-icon" sizes="57x57" href="/myschedule/img/apple-icon-57x57.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/myschedule/img/favicon-16x16.png">
