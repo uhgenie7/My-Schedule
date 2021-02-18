2ef4a4a241aaf05d2726c98f2bec7ca0b9d2545a
hyejin <dzzd777@naver.com>
2021-02-18 18:32:31 +0900

    수정: json url 변경 get으로 받아온 json을 파싱 작업을(JSON.parse) 하여 변경

--- js/total_avg.js
+++ js/total_avg.js
@@ -1,11 +1,13 @@
 $(function () {
   $.ajax({
-    url: '/myschedule/data/sch_rate.json',
+    url: '/myschedule/pages/read_json.php',
     success: function (result) {
-      let fwRate = Number(result[0].fw_rate);
-      let csRate = Number(result[0].cs_rate);
-      let plaRate = Number(result[0].pla_rate);
-      let renRate = Number(result[0].ren_rate);
+      let obj = JSON.parse(result);
+      console.log(obj);
+      let fwRate = Number(obj[0].fw_rate);
+      let csRate = Number(obj[0].cs_rate);
+      let plaRate = Number(obj[0].pla_rate);
+      let renRate = Number(obj[0].ren_rate);
 
       let schAvg = (fwRate * 0.2) + (csRate * 0.4) + (plaRate * 0.3) + (renRate * 0.1)
 
