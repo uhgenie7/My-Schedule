3e0059d132892c2128fc4dd307fffb3e00d6b232
hyejin <dzzd777@naver.com>
2021-02-18 10:33:00 +0900

    2/18 수정: min, max 값 적용 숫자 제한을 자바스크립트로 제한할 필요 없이 input 속성을 min 과 max로 0~100까지를 지정한다

--- include/grid_up.php
+++ include/grid_up.php
@@ -15,7 +15,7 @@
     <div class="pfContents">
       <h3>Database Project</h3>
       <p>
-        Progress Rate : <input type="number" maxlength="100" value="<?=$sch_db?>" class="rateNum" name="sch_db_rate">% || Last Update : <b><?=$db_date?></b>
+        Progress Rate : <input type="number" min="0" max="100" value="<?=$sch_db?>" class="rateNum" name="sch_db_rate">% || Last Update : <b><?=$db_date?></b>
       </p>
       <div class="pfBarBox">
         <span class="pfBar"></span>
@@ -29,7 +29,7 @@
     <div class="pfContents">
       <h3>API Project</h3>
       <p>
-        Progress Rate : <input type="number" maxlength="100" value="<?=$sch_api?>" class="rateNum" name="sch_api_rate">% || Last Update : <b><?=$api_date?></b>
+        Progress Rate : <input type="number" min="0" max="100" value="<?=$sch_api?>" class="rateNum" name="sch_api_rate">% || Last Update : <b><?=$api_date?></b>
       </p>
       <div class="pfBarBox">
         <span class="pfBar"></span>
@@ -43,7 +43,7 @@
     <div class="pfContents">
       <h3>Renewal Project</h3>
       <p>
-        Progress Rate :<input type="number" maxlength="100" value="<?=$sch_ren?>" class="rateNum" name="sch_ren_rate">% || Last Update : <b><?=$ren_date?></b>
+        Progress Rate :<input type="number" min="0" max="100" value="<?=$sch_ren?>" class="rateNum" name="sch_ren_rate">% || Last Update : <b><?=$ren_date?></b>
       </p>
       <div class="pfBarBox">
         <span class="pfBar"></span>
@@ -57,7 +57,7 @@
     <div class="pfContents">
       <h3>Web Planning Project</h3>
       <p>
-        Progress Rate : <input type="number" maxlength="100" value="<?=$sch_pla?>" class="rateNum" name="sch_pla_rate">% || Last Update : <b><?=$web_date?></b>
+        Progress Rate : <input type="number" min="0" max="100" value="<?=$sch_pla?>" class="rateNum" name="sch_pla_rate">% || Last Update : <b><?=$web_date?></b>
       </p>
       <div class="pfBarBox">
         <span class="pfBar"></span>
